d90b4e36ab5c5f5f7dd024c35f0998a4167b133f
Alifvia Putri <alifvia.dewani@mhs.unsoed.ac.id>
2025-12-07 22:57:09 +0700

    Update Member.java

--- pert4/Member.java
+++ pert4/Member.java
@@ -1,10 +1,19 @@
 class Member extends Customer {
-    // TODO: Tambahkan atribut tambahan
+    // Atribut tambahan khusus Member
+    private int poinReward;
+    private String level;
 
-    // TODO: Buat constructor dengan super
+    // Constructor menggunakan super
+    Member(String nama, String idCustomer, int totalBelanja, int poinReward, String level) {
+        super(nama, idCustomer, totalBelanja); // panggil constructor Customer
+        this.poinReward = poinReward;
+        this.level = level;
+    }
 
+    // Override method tampilkanInfo()
     @Override
     void tampilkanInfo() {
-        // TODO: panggil super, lalu tampilkan data tambahan
+        super.tampilkanInfo(); // tampilkan info Customer dulu
+        System.out.println("Poin Reward: " + poinReward + " | Level: " + level);
     }
 }
