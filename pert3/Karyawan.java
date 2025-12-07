d74acfeaf15eb4f082270882ffa4acb463140249
Alifvia Putri <alifvia.dewani@mhs.unsoed.ac.id>
2025-12-07 22:55:55 +0700

    Update Karyawan.java

--- pert3/Karyawan.java
+++ pert3/Karyawan.java
@@ -1,13 +1,16 @@
 class Karyawan {
-    // Deklarasikan variabel/state dasar (nama, gajiPokok)
-    // Gunakan access modifier yang tepat agar bisa diakses oleh subclass (misal: protected)
-    
-    
-    // Constructor untuk inisialisasi nama dan gajiPokok
-    
-    
-    // Method untuk menampilkan informasi dasar karyawan
+    // Variabel dasar (gunakan protected agar bisa diwarisi)
+    protected String nama;
+    protected double gajiPokok;
+
+    // Constructor
+    Karyawan(String nama, double gajiPokok) {
+        this.nama = nama;
+        this.gajiPokok = gajiPokok;
+    }
+
+    // Method untuk menampilkan info dasar
     void tampilInfo() {
-        
+        System.out.println("Nama: " + nama + " | Gaji Pokok: Rp " + gajiPokok);
     }
 }
